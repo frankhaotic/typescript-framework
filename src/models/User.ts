30fa4acd2b9dfa9d896c21bc4875b3864c599938
Frankie Flood <fsfloody@gmail.com>
2021-08-21 17:41:25 +0100

    re-integrate the Attributes class back into the User class

--- src/models/User.ts
+++ src/models/User.ts
@@ -8,6 +8,7 @@
 
 import { Eventing } from './Eventing';
 import { Sync } from './Sync';
+import { Attributes } from './Attributes';
 
 export interface UserProps {
   id?: number;
@@ -20,4 +21,9 @@ const rootUrl = 'http://localhost:3000/users';
 export class User {
   public events: Eventing = new Eventing();
   public sync: Sync<UserProps> = new Sync<UserProps>(rootUrl);
+  public attributes: Attributes<UserProps>;
+
+  constructor(attrs: UserProps) {
+    this.attributes = new Attributes<UserProps>(attrs);
+  }
 }
