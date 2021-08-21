8ca3a39ccbefb5e9cbb7a2b0f9ba89fb009c9576
Frankie Flood <fsfloody@gmail.com>
2021-08-21 17:36:47 +0100

    implement string typing and the keyof keyword in the Attributes class to allow us to get the correct returned type from the get() method

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,1 @@
 import { User } from './models/User';
-
-const user = new User({ name: 'ben', age: 53 });
-
-user.events.on('click', () => {
-  console.log('i have been clicked!');
-});
-
-user.events.trigger('click');
