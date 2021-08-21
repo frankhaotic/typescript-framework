4ed21caa44fd141a5174673bf10a292892920dea
Frankie Flood <fsfloody@gmail.com>
2021-08-21 17:13:13 +0100

    refactored the User and Sync classes to work together; used an interface to apply a constraint to the generic class Sync

--- src/models/Sync.ts
+++ src/models/Sync.ts
@@ -1,7 +1,10 @@
 import axios, { AxiosPromise } from 'axios';
-import { UserProps } from './User';
 
-export class Sync {
+interface HasId {
+  id?: number;
+}
+
+export class Sync<T extends HasId> {
   constructor(public rootUrl: string) {}
 
   fetch(id: number): AxiosPromise {
@@ -10,7 +13,7 @@ export class Sync {
 
   /* STUB with the save() method, we need to take care that we respect the entries in the database that already have an id; the Id is provided when the object enters the database, meaning that if it exists on the object that we are saving, we should be making a PUT request, because we'll be updating an existing entry in the db. Otherwise, we'll be making a POST request to enter a new object into the database */
 
-  save(data: UserProps): AxiosPromise {
+  save(data: T): AxiosPromise {
     const { id } = data;
 
     if (id) {
--- src/models/User.ts
+++ src/models/User.ts
@@ -7,6 +7,7 @@
  */
 
 import { Eventing } from './Eventing';
+import { Sync } from './Sync';
 
 export interface UserProps {
   id?: number;
@@ -14,8 +15,11 @@ export interface UserProps {
   age?: number;
 }
 
+const rootUrl = 'http://localhost:3000/users';
+
 export class User {
   public events: Eventing = new Eventing();
+  public sync: Sync<UserProps> = new Sync<UserProps>(rootUrl);
 
   constructor(private data: UserProps) {}
 
