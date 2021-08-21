8d7551d9ee00f98fb84652c0512697f78d5fb5dc
Frankie Flood <fsfloody@gmail.com>
2021-08-21 16:56:40 +0100

    refactored the sync class to a more generic version that accepts arguments specifically from the User class

--- src/models/Sync.ts
+++ src/models/Sync.ts
@@ -1,1 +1,24 @@
-export class Sync {}
+import axios, { AxiosPromise } from 'axios';
+import { UserProps } from './User';
+
+export class Sync {
+  constructor(public rootUrl: string) {}
+
+  fetch(id: number): AxiosPromise {
+    return axios.get(`${this.rootUrl}/${id}`);
+  }
+
+  /* STUB with the save() method, we need to take care that we respect the entries in the database that already have an id; the Id is provided when the object enters the database, meaning that if it exists on the object that we are saving, we should be making a PUT request, because we'll be updating an existing entry in the db. Otherwise, we'll be making a POST request to enter a new object into the database */
+
+  save(data: UserProps): AxiosPromise {
+    const { id } = data;
+
+    if (id) {
+      // put request
+      return axios.put(`${this.rootUrl}/${id}`, data);
+    } else {
+      // post
+      return axios.post(this.rootUrl, data);
+    }
+  }
+}
