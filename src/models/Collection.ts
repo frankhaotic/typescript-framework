3c025a4802e66459b1d20fe2422926beccfd944e
Frankie Flood <fsfloody@gmail.com>
2021-08-24 15:41:31 +0100

    add a collection view component that will create a collection of all things inside of the db, and then render then onto the screen

--- src/models/Collection.ts
+++ src/models/Collection.ts
@@ -18,10 +18,10 @@ export class Collection<T, K> {
   fetch(): void {
     axios.get(this.rootUrl).then((response: AxiosResponse) => {
       response.data.forEach((value: K) => {
-        this.models.push(this.deserialize(value));
+        const item = this.deserialize(value);
+        this.models.push(item);
       });
+      this.trigger('change');
     });
-
-    this.trigger('change');
   }
 }
